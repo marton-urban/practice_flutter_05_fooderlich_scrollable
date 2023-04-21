56777da34da839358036415617ecc57c1fa9a425
Marton Urban <urban.marton@gmail.com>
2023-04-21 12:44:52 +0200

    add top/bottom scroll listener

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
-  final mockService = MockFooderlichService();
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({super.key});
 
-  ExploreScreen({super.key});
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +30,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: _controller,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
@@ -28,5 +43,24 @@ class ExploreScreen extends StatelessWidget {
         }
       },
     );
+  }
+
+  void _scrollListener() {
+    // 1
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      log('i am at the bottom!');
+    }
+    // 2
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      log('i am at the top!');
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListener);
+    super.dispose();
   }
 }
